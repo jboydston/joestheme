43167102206be7474a71fb36a70264067917f285
Dean <deanroyal@gmail.com>
2013-08-28 08:14:26 -0700

    added searchpage.php to joestheme

---
+++ searchpage.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Search Page
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<?php get_search_form(); ?>
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
